c07f9f7615f9ec9d500800ccf50c22fbc7a48c49
dopadream <slyshidiscord@gmail.com>
2025-05-15 22:20:07 -0500

    Initial commit

---
+++ common/src/main/java/io/github/dopadream/ExampleMod.java
@@ -0,0 +1,9 @@
+package io.github.dopadream;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "mineflac";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
